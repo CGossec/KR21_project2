ae1ecf0ef07ddacb96a6e671c9c4270f4f562206
Celian GOSSEC <celian.gossec@gmail.com>
2021-12-09 10:09:02 +0100

    Add graph pruning to BNReasoner + small main to test it

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,4 +1,7 @@
-from typing import Union
+import copy
+import os
+from typing import List, Union
+
 from BayesNet import BayesNet
 
 
@@ -15,4 +18,35 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should go
+    def pruning(self, X: List[str], Y: List[str], Z: List[str]) -> BayesNet:
+        """
+        Given a graph G and disjoint set of nodes X, Y, Z, return a pruned graph G' such that:
+            * There are no leaf nodes that do not belong to X, Y, Z
+            * All edges outgoing from nodes in Z are deleted
+
+        :param X: List of nodes
+        :param Y: List of nodes
+        :param Z: List of nodes
+        :return: Copy of self.net, with appropriate nodes and edge removed
+        """
+        pruned_graph = copy.copy(self.bn)
+        modified = True
+        while modified:
+            modified = False
+            for var in pruned_graph.get_all_variables():
+                if pruned_graph.get_children(var) == [] and var not in X and var not in Y and var not in Z:
+                    pruned_graph.del_var(var)
+                    modified = True
+            for var in Z:
+                for child in pruned_graph.get_children(var):
+                    pruned_graph.del_edge((var, child))
+                    modified = True
+        return pruned_graph
+
+
+if __name__ == "__main__":
+    bifxml_path = os.getcwd() + "/testing/dog_problem.BIFXML"
+    bnr = BNReasoner(bifxml_path)
+    bnr.bn.draw_structure()
+    pruned = bnr.pruning(["light-on"], ["family-out"], ["dog-out"])
+    pruned.draw_structure()
