c777cf71b481daf99d22a5faee1c704df05ecce2
Celian GOSSEC <celian.gossec@gmail.com>
2021-12-09 14:49:53 +0100

    Update CPTs in network pruning

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,6 +1,6 @@
 import copy
 import os
-from typing import List, Union
+from typing import List, Union, Dict
 
 from BayesNet import BayesNet
 
@@ -18,28 +18,38 @@ class BNReasoner:
         else:
             self.bn = net
 
-    def pruning(self, X: List[str], Y: List[str], Z: List[str]) -> BayesNet:
+    def pruning(self, query: List[str], evidence: Dict[str, bool]) -> BayesNet:
         """
-        Given a graph G and disjoint set of nodes X, Y, Z, return a pruned graph G' such that:
-            * There are no leaf nodes that do not belong to X, Y, Z
-            * All edges outgoing from nodes in Z are deleted
+        Given a graph G and disjoint set of nodes in the query and an evidence, return a pruned graph G' such that:
+            * There are no leaf nodes that do not belong to Query + evidence
+            * All edges outgoing from nodes in evidence are deleted
 
-        :param X: List of nodes
-        :param Y: List of nodes
-        :param Z: List of nodes
-        :return: Copy of self.net, with appropriate nodes and edge removed
+        :param query: List of nodes
+        :param evidence: Dictionary of variables with their assigned truth values
+        :return: Copy of self.net, with appropriate nodes and edge removed, and CPTs updated
         """
         pruned_graph = copy.copy(self.bn)
         modified = True
         while modified:
             modified = False
             for var in pruned_graph.get_all_variables():
-                if pruned_graph.get_children(var) == [] and var not in X and var not in Y and var not in Z:
+                if pruned_graph.get_children(var) == [] and var not in query and var not in evidence:
                     pruned_graph.del_var(var)
                     modified = True
-            for var in Z:
-                for child in pruned_graph.get_children(var):
-                    pruned_graph.del_edge((var, child))
+            CPTs = pruned_graph.get_all_cpts()
+            for given in evidence:
+                for variable in CPTs:
+                    cpt = CPTs[variable]
+                    if given not in cpt.columns:
+                        continue
+                    if cpt.columns[-2] == given:
+                        continue
+                    indices_to_drop = cpt[cpt[given] == (not evidence[given])].index
+                    new_cpt = cpt.drop(indices_to_drop)
+                    new_cpt = new_cpt.drop(given, axis = 1)
+                    pruned_graph.update_cpt(variable, new_cpt)
+                for child in pruned_graph.get_children(given):
+                    pruned_graph.del_edge((given, child))
                     modified = True
         return pruned_graph
 
@@ -47,6 +57,6 @@ class BNReasoner:
 if __name__ == "__main__":
     bifxml_path = os.getcwd() + "/testing/dog_problem.BIFXML"
     bnr = BNReasoner(bifxml_path)
-    bnr.bn.draw_structure()
-    pruned = bnr.pruning(["light-on"], ["family-out"], ["dog-out"])
-    pruned.draw_structure()
+    print(bnr.bn.get_all_cpts())
+    pruned = bnr.pruning(["light-on", "family-out"], {"dog-out" : False})
+    print(pruned.get_all_cpts())
